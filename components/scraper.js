687ef9ffcbb50fa454d903678583f7d1b2d63878
docdoom815 <ronald.crecco@gmail.com>
2020-01-10 15:53:55 -0500

    game titles scraped

--- components/scraper.js
+++ components/scraper.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList, ScrollView } from "react-native";
 import Axios from "axios";
-import cheerio from 'cheerio';
-
+const cheerio = require('react-native-cheerio');
+var HTMLParser = require('fast-html-parser');
 
 export default class Scraper extends React.Component {
     constructor(props) {
@@ -17,36 +17,79 @@ export default class Scraper extends React.Component {
         return Axios.get("https://www.metacritic.com/feature/major-upcoming-video-game-release-dates-xbox-ps4-pc-switch")
             .then(response => {
 
-                let $ = cheerio.load(response.data);
-
-                let articles = [];
-
-                $('-pklmxa').each(() => {
-
-                    let head = $(this)
-                        .find('span')
 
 
+                var root = HTMLParser.parse(response.data);
+
+                let linetable = root.querySelectorAll('caption');
+
+                let games = [];
+
+                console.log(root);
+                console.log(root.querySelectorAll('caption'));
+
+                // linetable[0].text
+
+                linetable.forEach((table) => {
+                    console.log(table.text)
+
+                    games.push(table.text);
 
                 })
 
+                return games;
 
-                debugger
-                return response.data;
-            }).then(response => {
+
+                ;
+
+
+
+
+                // let $ = cheerio.load(response.data);
+
+                // let games = [];
+
+                // $('.linedtable').each(() => {
+
+
+                //     let title = $(this).find('caption').text();
+
+                //     console.log(title)
+
+                //     games.push(title);
+
+            })
+            // return response.data
+            // return games;
+            .then(response => {
                 this.setState({
                     data: response
                 })
             })
+
+        // .catch(function (err) {
+        //     return err;
+        // });
+
     }
     componentDidMount() {
         this.scrape()
     }
 
     render() {
+
+        list = <FlatList>
+
+
+
+        </FlatList>
+
+
         return (
             <View>
-                <Text>{this.state.data.toString()}</Text>
+                <ScrollView>
+                    <Text>{this.state.data}</Text>
+                </ScrollView>
             </View>
         )
     }
